b310010a751c414dd5d635c700d17a6fb284578e
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-07-12 16:59:38 -0500

    update store reducer imports and connect add method to form onSubmit

--- list-app/src/app/store.js
+++ list-app/src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import listReducer from '../features/list/listSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    list: listReducer,
   },
 });
--- list-app/src/features/form/AddItemForm.js
+++ list-app/src/features/form/AddItemForm.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { add } from '../list/listSlice'
 
 export const AddItemForm = () => {
+
+  const [item, setItem] = useState('')
+
+  const addItem = (e) => {
+    e.preventDefault()
+    add(item)
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={addItem}>
         <label>Item</label>
-        <input type="text" placeholder="Item" />
+        <input type="text" placeholder="Item" onChange={(e) => setItem(e.target.value)} />
         <button type='submit' value="Add" />
       </form>
     </div>
